12bf5baf282272333657c78f2bcdfc87665d4e0a
teric622 <tlasecaeric@gmail.com>
2021-05-03 16:59:37 -0400

    implement icons

---
+++ eventbycatagory.js
@@ -0,0 +1,12 @@
+// need function and algorithm for :
+// updating the cards and actually showing and filling the box when the user picks 
+// certain specifications i.e category and week 
+// When user goes on to search for a specific event and only showing that event
+// putting their favorits on list
+// WITH JSON NEED : GET IMAGE AND UPDATE IMAGE TO FILL CARD
+// GET EVENT NAME
+// DATE AND TIME AND FIND A WAY OF ATTACHING FOR WHEN USER PICKS THE WEEK AND TYPE OF EVENT
+// ON CLICK WHEN USER CLICKS A CARD, IT HAS TO SEND THEM TO THAT EVENT, 
+// MAKE A UNIQUE ID FOR EACH EVENT?
+// READ USERS SEARCH 
+// READ USERS CATEGORY SELECTION AND WEEK SELECTION
